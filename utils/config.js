ecc40590e6d174776861c9338cd0324a6ed35de8
Alen Mandic <alenmandic1@gmail.com>
2024-07-09 19:18:32 +0200

    testing end-to-end tests in workflow

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3003
 
 const MONGODB_URL = process.env.NODE_ENV === 'TEST' ?
-  process.env.TESTING_URL : process.env.MONGODB_URL // testing URL changed to work in github actions workflow alongside github action secrets
+  process.env.MONGODB_TEST_URI : process.env.MONGODB_URL // testing URL changed to work in github actions workflow alongside github action secrets
 
 module.exports = { PORT, MONGODB_URL }
